b73b68e05fc60d9e71240f5499420672df759f42
Stefan Larsson <stefan.larsson@combine.se>
2016-02-22 16:58:34 +0100

    Added router.

--- src/main/scala/org/scalajs/openui5/sap/ui/Ui.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/Ui.scala
@@ -1,5 +1,6 @@
 package org.scalajs.openui5.sap.ui
 
+import org.scalajs.openui5.sap.ui.core.Core
 import org.scalajs.openui5.sap.ui.core.mvc.{Controller, View}
 
 import scala.scalajs.js
@@ -16,4 +17,6 @@ object Ui extends js.Object {
   def view(id: String): View = js.native
   def view(view: String | js.Any): View = js.native
   def view(id: String, view: String | js.Any): View = js.native
+
+  def getCore(): Core = js.native
 }
