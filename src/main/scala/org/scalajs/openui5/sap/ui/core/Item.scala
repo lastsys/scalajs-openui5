8138dfbe3cfe566942beda9b250336ac046a3fed
Stefan Larsson <stefan.larsson@combine.se>
2016-02-23 09:15:51 +0100

    Added ListItemSettingsBuilder with dependencies.

--- src/main/scala/org/scalajs/openui5/sap/ui/core/Item.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/core/Item.scala
@@ -1,11 +1,25 @@
 package org.scalajs.openui5.sap.ui.core
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.{ScalaJSDefined, JSName}
+
+@ScalaJSDefined
+trait ItemSettings extends ElementSettings
+object ItemSettings extends ItemSettingsBuilder[ItemSettings]
+class ItemSettingsBuilder[T <: js.Object] extends ElementSettingsBuilder[T] {
+  def text(v: String) = setting("text", v)
+  def enabled(v: Boolean) = setting("enabled", v)
+  def textDirection(v: TextDirection) = setting("textDirection", v)
+  def key(v: String) = setting("key", v)
+}
 
 @JSName("sap.ui.core.Item")
 @js.native
-class Item extends Element {
+class Item(id: js.UndefOr[String] = js.native,
+           settings: js.UndefOr[ItemSettings] = js.native) extends Element {
+  def this(id: String) = this(id, js.undefined)
+  def this(settings: ItemSettings) = this(js.undefined, settings)
+
   def getKey(): String = js.native
   def getText(): String = js.native
 
--- src/main/scala/org/scalajs/openui5/sap/ui/core/ListItem.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/core/ListItem.scala
@@ -1,11 +1,22 @@
 package org.scalajs.openui5.sap.ui.core
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.{ScalaJSDefined, JSName}
+
+@ScalaJSDefined
+trait ListItemSettings extends ItemSettings
+object ListItemSettings extends ListItemSettingsBuilder[ListItemSettings]
+class ListItemSettingsBuilder[T <: js.Object] extends ItemSettingsBuilder[T] {
+  def icon(v: String) = setting("icon", v)
+  def additionalText(v: String) = setting("additionalText", v)
+}
 
 @JSName("sap.ui.core.ListItem")
 @js.native
-class ListItem(id: String = js.native,
-               settings: js.Dynamic = js.native) extends Item {
+class ListItem(id: js.UndefOr[String] = js.native,
+               settings: js.UndefOr[ListItemSettings] = js.native)
+  extends Item {
 
+  def this(id: String) = this(id, js.undefined)
+  def this(settings: ListItemSettings) = this(js.undefined, settings)
 }
