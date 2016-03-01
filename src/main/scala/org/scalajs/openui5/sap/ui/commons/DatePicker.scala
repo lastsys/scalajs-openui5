14d0c41768774b951526d3bcc9c334e2a74ce244
Stefan Larsson <stefan.larsson@combine.se>
2016-03-01 08:26:31 +0100

    Added settings to DatePicker.

--- src/main/scala/org/scalajs/openui5/sap/ui/commons/DatePicker.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/commons/DatePicker.scala
@@ -1,11 +1,33 @@
 package org.scalajs.openui5.sap.ui.commons
 
+import org.scalajs.openui5.util.{Settings, SettingsMap, noSettings}
+
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.{ScalaJSDefined, JSName}
+
+@ScalaJSDefined
+trait DatePickerSettings extends TextFieldSettings
+object DatePickerSettings extends DatePickerSettingsBuilder(noSettings)
+
+class DatePickerSettingsBuilder(val dict: SettingsMap)
+  extends Settings[DatePickerSettings, DatePickerSettingsBuilder](new DatePickerSettingsBuilder(_))
+    with DatePickerSetters[DatePickerSettings, DatePickerSettingsBuilder]
+
+trait DatePickerSetters[T <: js.Object, B <: Settings[T,_]]
+  extends TextFieldSetters[T, B] {
+
+  def locale(v: String) = setting("locale", v)
+  def yyyymmdd(v: String) = setting("yyyymmdd", v)
+}
 
 @JSName("sap.ui.commons.DatePicker")
 @js.native
-class DatePicker(id: String = js.native,
-                 settings: js.Dynamic = js.native) extends TextField {
+class DatePicker(id: js.UndefOr[String] = js.native,
+                 settings: js.UndefOr[DatePickerSettings] = js.native)
+  extends TextField {
+
+  def this(id: String) = this(id, js.undefined)
+  def this(settings: DatePickerSettings) = this(js.undefined, settings)
+
   def getYyyymmdd(): String = js.native
 }
