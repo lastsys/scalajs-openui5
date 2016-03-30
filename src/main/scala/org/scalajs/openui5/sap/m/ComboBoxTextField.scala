46674017922c3edc33a6f856cb347675f160c848
Stefan Larsson <stefan.larsson@combine.se>
2016-03-30 14:32:23 +0200

    Added placeholder for MultiComboBox.

---
+++ src/main/scala/org/scalajs/openui5/sap/m/ComboBoxTextField.scala
@@ -0,0 +1,10 @@
+package org.scalajs.openui5.sap.m
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@JSName("sap.m.ComboBoxTextField")
+@js.native
+class ComboBoxTextField extends InputBase {
+
+}
