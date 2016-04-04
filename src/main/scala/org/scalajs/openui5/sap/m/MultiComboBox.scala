d36d6cf4802dee3abc611964b6a4a182bf82cdfa
Stefan Larsson <stefan.larsson@combine.se>
2016-04-04 10:25:42 +0200

    Added setSelectedKeys to MultiComboBox.

--- src/main/scala/org/scalajs/openui5/sap/m/MultiComboBox.scala
+++ src/main/scala/org/scalajs/openui5/sap/m/MultiComboBox.scala
@@ -3,6 +3,12 @@ package org.scalajs.openui5.sap.m
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSName
 
+/** The MultiComboBox control provides a list box with items and a text field
+  * allowing the user to either type a value directly into the control or
+  * choose from the list of existing items.
+  *
+  * @since 1.22.0
+  */
 @JSName("sap.m.MultiComboBox")
 @js.native
 class MultiComboBox extends ComboBoxBase {
@@ -10,4 +16,6 @@ class MultiComboBox extends ComboBoxBase {
   def attachSelectionFinish(function: js.Function): this.type = js.native
 
   def getSelectedKeys(): js.Array[String] = js.native
+
+  def setSelectedKeys(selectedKeys: js.Array[String]): this.type = js.native
 }
