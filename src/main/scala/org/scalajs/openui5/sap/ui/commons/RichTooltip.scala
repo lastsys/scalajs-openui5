559e4d35e776e4f84a709612fb025e61faab86a3
Stefan Larsson <stefan.larsson@combine.se>
2016-04-12 13:56:14 +0200

    Added RichTooltip.

---
+++ src/main/scala/org/scalajs/openui5/sap/ui/commons/RichTooltip.scala
@@ -0,0 +1,34 @@
+package org.scalajs.openui5.sap.ui.commons
+
+import org.scalajs.openui5.sap.ui.core.{TooltipBase, TooltipBaseSetters, TooltipBaseSettings, URI}
+import org.scalajs.openui5.util.{Settings, SettingsMap, noSettings}
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
+
+@ScalaJSDefined
+trait RichTooltipSettings extends TooltipBaseSettings
+object RichTooltipSettings extends RichTooltipSettingsBuilder(noSettings)
+
+class RichTooltipSettingsBuilder(val dict: SettingsMap)
+  extends Settings[RichTooltipSettings, RichTooltipSettingsBuilder](new RichTooltipSettingsBuilder(_))
+    with RichTooltipSetters[RichTooltipSettings, RichTooltipSettingsBuilder]
+
+trait RichTooltipSetters[T <: js.Object, B <: Settings[T,_]]
+  extends TooltipBaseSetters[T, B] {
+
+  def title(v: String) = setting("title", v)
+  def imageSrc(v: URI) = setting("imageSrc", v)
+  def valueStateText(v: String) = setting("valueStateText", v)
+  def imageAltText(v: String) = setting("imageAltText", v)
+}
+
+@JSName("sap.ui.commons.RichTooltip")
+@js.native
+class RichTooltip(id: js.UndefOr[String] = js.native,
+                  settings: js.UndefOr[RichTooltipSettings])
+  extends TooltipBase {
+
+  def this(id: String) = this(id, js.undefined)
+  def this(settings: RichTooltipSettings) = this(js.undefined, settings)
+}
--- src/main/scala/org/scalajs/openui5/sap/ui/core/TooltipBase.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/core/TooltipBase.scala
@@ -1,10 +1,38 @@
 package org.scalajs.openui5.sap.ui.core
 
+import org.scalajs.openui5.util.{Settings, SettingsMap, noSettings}
+
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
+
+@ScalaJSDefined
+trait TooltipBaseSettings extends ControlSettings
+object TooltipBaseSettings extends TooltipBaseSettingsBuilder(noSettings)
+
+class TooltipBaseSettingsBuilder(val dict: SettingsMap)
+  extends Settings[TooltipBaseSettings, TooltipBaseSettingsBuilder](new TooltipBaseSettingsBuilder(_))
+    with TooltipBaseSetters[TooltipBaseSettings, TooltipBaseSettingsBuilder]
+
+trait TooltipBaseSetters[T <: js.Object, B <: Settings[T,_]]
+  extends ControlSetters[T, B] {
+
+  def text(v: String) = setting("text", v)
+  def openDuration(v: Int) = setting("openDuration", v)
+  def closeDuration(v: Int) = setting("closeDuration", v)
+  def myPosition(v: Dock) = setting("myPosition", v)
+  def atPosition(v: Dock) = setting("atPosition", v)
+  def offset(v: String) = setting("offset", v)
+  def collision(v: Collision) = setting("collision", v)
+  def openDelay(v: Int) = setting("openDelay", v)
+  def closeDelay(v: Int) = setting("closeDelay", v)
+}
 
 @JSName("sap.ui.core.TooltipBase")
 @js.native
-trait TooltipBase extends Control {
+class TooltipBase(id: js.UndefOr[String] = js.native,
+                  settings: js.UndefOr[TooltipBaseSettings] = js.native)
+  extends Control {
 
+  def this(id: String) = this(id, js.undefined)
+  def this(settings: TooltipBaseSettings) = this(js.undefined, settings)
 }
