0bc5f4d1f71f8bbfac0122453ae563e7717c11ad
Stefan Larsson <stefan.larsson@combine.se>
2016-04-14 12:12:38 +0200

    Reformatted indentation.

--- src/main/scala/org/scalajs/openui5/sap/ui/commons/RichTooltip.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/commons/RichTooltip.scala
@@ -6,29 +6,34 @@ import org.scalajs.openui5.util.{Settings, SettingsMap, noSettings}
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
 
-  @ScalaJSDefined
-  trait RichTooltipSettings extends TooltipBaseSettings
-  object RichTooltipSettings extends RichTooltipSettingsBuilder(noSettings)
+@ScalaJSDefined
+trait RichTooltipSettings extends TooltipBaseSettings
 
-  class RichTooltipSettingsBuilder(val dict: SettingsMap)
-    extends Settings[RichTooltipSettings, RichTooltipSettingsBuilder](new RichTooltipSettingsBuilder(_))
-      with RichTooltipSetters[RichTooltipSettings, RichTooltipSettingsBuilder]
+object RichTooltipSettings extends RichTooltipSettingsBuilder(noSettings)
 
-  trait RichTooltipSetters[T <: js.Object, B <: Settings[T,_]]
-    extends TooltipBaseSetters[T, B] {
+class RichTooltipSettingsBuilder(val dict: SettingsMap)
+  extends Settings[RichTooltipSettings, RichTooltipSettingsBuilder](new RichTooltipSettingsBuilder(_))
+    with RichTooltipSetters[RichTooltipSettings, RichTooltipSettingsBuilder]
 
-    def title(v: String) = setting("title", v)
-    def imageSrc(v: URI) = setting("imageSrc", v)
-    def valueStateText(v: String) = setting("valueStateText", v)
-    def imageAltText(v: String) = setting("imageAltText", v)
-  }
+trait RichTooltipSetters[T <: js.Object, B <: Settings[T, _]]
+  extends TooltipBaseSetters[T, B] {
 
-  @JSName("sap.ui.commons.RichTooltip")
-  @js.native
-  class RichTooltip(id: js.UndefOr[String] = js.native,
-                    settings: js.UndefOr[RichTooltipSettings])
-    extends TooltipBase {
+  def title(v: String) = setting("title", v)
 
-    def this(id: String) = this(id, js.undefined)
-    def this(settings: RichTooltipSettings) = this(js.undefined, settings)
-  }
+  def imageSrc(v: URI) = setting("imageSrc", v)
+
+  def valueStateText(v: String) = setting("valueStateText", v)
+
+  def imageAltText(v: String) = setting("imageAltText", v)
+}
+
+@JSName("sap.ui.commons.RichTooltip")
+@js.native
+class RichTooltip(id: js.UndefOr[String] = js.native,
+                  settings: js.UndefOr[RichTooltipSettings])
+  extends TooltipBase {
+
+  def this(id: String) = this(id, js.undefined)
+
+  def this(settings: RichTooltipSettings) = this(js.undefined, settings)
+}
