bfe78072fbd5162ebcef01881aab5b7d207309ab
Stefan Larsson <stefan.larsson@combine.se>
2016-04-25 10:14:18 +0200

    Added methods to Message.

--- src/main/scala/org/scalajs/openui5/sap/ui/core/Message.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/core/Message.scala
@@ -9,4 +9,17 @@ class Message(id: js.UndefOr[String] = js.native,
               settings: js.UndefOr[js.Any] = js.native) extends Element {
   def this(id: String) = this(id, js.undefined)
   def this(settings: js.Any) = this(js.undefined, settings)
+
+  def compareByType(other: Message): Int = js.native
+  def getDefaultIcon(size: String): URI = js.native
+  def getIcon(): URI = js.native
+  def getLevel(): MessageType = js.native
+  def getReadOnly(): Boolean = js.native
+  def getText(): String = js.native
+  def getTimestamp(): String = js.native
+  def setIcon(icon: URI): this.type = js.native
+  def setLevel(level: MessageType): this.type = js.native
+  def setReadOnly(readOnly: Boolean): this.type = js.native
+  def setText(text: String): this.type = js.native
+  def setTimestamp(timestamp: String): this.type = js.native
 }
