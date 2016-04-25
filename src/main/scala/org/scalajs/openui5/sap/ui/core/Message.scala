547c9c1649670695d8a9b3327c1c10d05b389b60
Stefan Larsson <stefan.larsson@combine.se>
2016-04-25 10:23:15 +0200

    Added settings to Message.

--- src/main/scala/org/scalajs/openui5/sap/ui/core/Message.scala
+++ src/main/scala/org/scalajs/openui5/sap/ui/core/Message.scala
@@ -1,7 +1,27 @@
 package org.scalajs.openui5.sap.ui.core
 
+import org.scalajs.openui5.util.{Settings, SettingsMap, noSettings}
+
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
+
+@ScalaJSDefined
+trait MessageSettings extends ElementSettings
+object MessageSettings extends MessageSettingsBuilder(noSettings)
+
+class MessageSettingsBuilder(val dict: SettingsMap)
+  extends Settings[MessageSettings, MessageSettingsBuilder](new MessageSettingsBuilder(_))
+    with MessageSetters[MessageSettings, MessageSettingsBuilder]
+
+trait MessageSetters[T <: js.Object, B <: Settings[T, _]]
+  extends ElementSetters[T, B] {
+
+  def text(v: String) = setting("text", v)
+  def timestamp(v: String) = setting("timestamp", v)
+  def icon(v: URI) = setting("icon", v)
+  def level(v: MessageType) = setting("level", v)
+  def readOnly(v: Boolean) = setting("readOnly", v)
+}
 
 @JSName("sap.ui.core.Message")
 @js.native
