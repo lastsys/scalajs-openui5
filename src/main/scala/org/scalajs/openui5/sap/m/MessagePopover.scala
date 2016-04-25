d7e64ac74c4e599db33f5f94011537bef9135e7b
Stefan Larsson <stefan.larsson@combine.se>
2016-04-25 11:05:51 +0200

    Added addItem to MessagePopover.

--- src/main/scala/org/scalajs/openui5/sap/m/MessagePopover.scala
+++ src/main/scala/org/scalajs/openui5/sap/m/MessagePopover.scala
@@ -43,6 +43,7 @@ class MessagePopover(id: js.UndefOr[String] = js.native,
   def this(id: String) = this(id, js.undefined)
   def this(settings: MessagePopoverSettings) = this(js.undefined, settings)
 
+  def addItem(item: MessagePopoverItem): this.type = js.native
   def close(): this.type = js.native
   def destroyItems(): this.type = js.native
   def isOpen(): Boolean = js.native
