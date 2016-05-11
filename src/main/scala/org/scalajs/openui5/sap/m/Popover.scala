b7865bdd73bf90e176cb70808cef7c4558d37536
Stefan Larsson <stefan.larsson@combine.se>
2016-05-11 08:55:44 +0200

    Added close to Popover.

--- src/main/scala/org/scalajs/openui5/sap/m/Popover.scala
+++ src/main/scala/org/scalajs/openui5/sap/m/Popover.scala
@@ -56,5 +56,6 @@ class Popover(id: js.UndefOr[String] = js.native,
   def openBy(control: Control,
              skipInstanceManager: js.UndefOr[Boolean] = js.native): this.type = js.native
 
+  def close(): this.type = js.native
   def setFooter(footer: Control): this.type = js.native
 }
