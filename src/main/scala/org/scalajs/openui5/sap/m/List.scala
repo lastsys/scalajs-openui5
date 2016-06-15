74e35ecb96d66890f442a5d849fec7e7e039a6a2
Stefan Larsson <stefan.larsson@combine.se>
2016-06-15 13:51:31 +0200

    Added methods to List.

--- src/main/scala/org/scalajs/openui5/sap/m/List.scala
+++ src/main/scala/org/scalajs/openui5/sap/m/List.scala
@@ -6,5 +6,9 @@ import scala.scalajs.js.annotation.JSName
 @JSName("sap.m.List")
 @js.native
 class List extends ListBase {
-
+  def setSelectedItem(listItem: ListItemBase,
+                      select: Boolean = true): this.type = js.native
+  def setSelectedItemById(id: String,
+                          select: Boolean = true): this.type = js.native
+  def getItems(): js.Array[_ <: ListItemBase] = js.native
 }
