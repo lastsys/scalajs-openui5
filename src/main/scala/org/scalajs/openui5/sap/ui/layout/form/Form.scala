988d4c23482cae0879b6e71370b8163e713e6752
Stefan Larsson <stefan.larsson@combine.se>
2016-07-05 07:51:08 +0200

    Added placeholder for Form.

---
+++ src/main/scala/org/scalajs/openui5/sap/ui/layout/form/Form.scala
@@ -0,0 +1,12 @@
+package org.scalajs.openui5.sap.ui.layout.form
+
+import org.scalajs.openui5.sap.ui.core.Control
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@JSName("sap.ui.layout.form.Form")
+@js.native
+class Form extends Control {
+
+}
